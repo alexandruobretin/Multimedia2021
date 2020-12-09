bed3f149f15ce19368f5f5c1f4c73fa569effc41
Alexandru Obretin <alexandru.obretin93@gmail.com>
2020-12-09 09:39:34 +0200

    1093 - s11: device orientation. quick note: at line 1089 should be 'compass.style.transform = rotate(-alphadeg)' between double quotes, not with paratheses like in the recording

---
+++ 1093/s11/geo.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('geo.html')    
+
+
+if __name__ == '__main__':
+    app.run(debug=True, ssl_context='adhoc', host='192.168.1.7', port=8030)
